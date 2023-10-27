0e4f8f17f494c7c3be0a7223d6eec19751a4f0e5
傅子湘 <fuzx@xmpaoyou.com>
2023-10-27 14:29:36 +0800

    feat: add gh-pages

---
+++ gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm run build
+
+# 切换到dist目录
+cd dist
+
+# 初始化git仓库
+git init
+
+# 添加所有文件到暂存区
+git add .
+
+# 提交更改
+git commit -m "publish"
+
+git  branch -m main gh-pages
+
+# 添加远程仓库
+git remote add origin git@github.com:fzxiang/webcontainers-express-app.git
+
+# 推送到远程仓库的gh-pages分支
+git push -u origin gh-pages -f
